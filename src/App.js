e019227645ee8e60e98ddc8531fcc719e329bd32
tanyakz <tshirazi@gmail.com>
2020-02-05 18:52:05 -0500

    update our menu-items to navigate wiRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import  HomePage from './pages/homepage/Homepage.component';
 
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return  <div>
-  <HomePage></HomePage>
+  <Switch>
+    <Route exact path='/' component={HomePage}/>
+    <Route exact path='/hats' component={HatsPage}/>
+  </Switch>
+  
   </div>;
 }
 
