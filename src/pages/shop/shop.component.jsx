b00901b6ebe30190098bad0bec2b3fc45b3c7752
tanyakz <tshirazi@gmail.com>
2020-02-08 12:33:28 -0500

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SHOP_DATE from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            collections : SHOP_DATE
+        };
+    }
+    render(){
+        const {collections} = this.state;
+        return (<div className='shop-page'>
+         { collections.map( ({id, ...OtherCollectionProps}) => (
+             <CollectionPreview key={collections.id} {...OtherCollectionProps}/>
+         )) }
+        </div>)
+    }
+}
+
+export default ShopPage;
